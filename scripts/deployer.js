a587d28aca2da9c33df51f43f25b7e63e9a16419
Fernando Ribas <fribas84@gmail.com>
2023-10-23 16:05:24 -0300

    disbled es-lit for deployer file

--- scripts/deployer.js
+++ scripts/deployer.js
@@ -1,42 +1,44 @@
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
 // We require the Hardhat Runtime Environment explicitly here. This is optional
+// eslint-disable-next-line max-len
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require('hardhat');
 
 async function main() {
-  const frontDoorWallet = "0xb29bd8e0E273843AaA92BEAC6DbE0EC788e1852f";
+  const frontDoorWallet = '0xb29bd8e0E273843AaA92BEAC6DbE0EC788e1852f';
 
-  const fndrToken = await hre.ethers.deployContract("FrontDoorToken", []);
+  const fndrToken = await hre.ethers.deployContract('FrontDoorToken', []);
   await fndrToken.waitForDeployment();
   const fndrTokenAddress = fndrToken.target;
-  console.log("Deploying Contracts...");
-  console.log("Front Door Token deployed to: ", fndrTokenAddress);
+  console.log('Deploying Contracts...');
+  console.log('Front Door Token deployed to: ', fndrTokenAddress);
 
-  const fndrFaucet = await hre.ethers.deployContract("FNDR_Faucet", [
+  const fndrFaucet = await hre.ethers.deployContract('FNDR_Faucet', [
     fndrTokenAddress,
   ]);
   await fndrFaucet.waitForDeployment();
   const fndrFaucetAddress = fndrFaucet.target;
 
-  console.log("Front Door Faucet deployed to: ", fndrFaucetAddress);
+  console.log('Front Door Faucet deployed to: ', fndrFaucetAddress);
 
-  console.log("Configuring roles in Front Door Token...");
+  console.log('Configuring roles in Front Door Token...');
 
   await fndrToken.setFaucet(fndrFaucetAddress);
 
-  const recruitment = await hre.ethers.deployContract("Recruitment", [
+  const recruitment = await hre.ethers.deployContract('Recruitment', [
     fndrTokenAddress,
     frontDoorWallet,
   ]);
   await recruitment.waitForDeployment();
   console.log(
-    "Front Door Recruiter Contract deployed to: ",
-    recruitment.target
+      'Front Door Recruiter Contract deployed to: ',
+      recruitment.target,
   );
-
 }
 
 // We recommend this pattern to be able to use async/await everywhere
